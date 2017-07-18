73be0216ae9e8bb95130135bb6e99b8d4d908acc
Joerg Roth <info@leylines.ch>
2017-07-18 20:07:03 +0200

    update 18.7.2017

--- source/scripts/contact_me.js
+++ source/scripts/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "https://formspree.io/simone@naturapraxis.ch",
+                url: "https://formspree.io/info@leylines.ch",
                 type: "POST",
                 data: {
                     name: name,
